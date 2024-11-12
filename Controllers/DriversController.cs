cc5076c51e5bcf0da2ca1dd10252946b3c1a1024
arewezaetoime <hristoiliev.1994@gmail.com>
2024-11-12 20:06:20 +0200

    Initial commit

---
+++ Controllers/DriversController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1_Racing_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+    }
+}
