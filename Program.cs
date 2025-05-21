3bab8249d40fa50ccac5dbd07f64291afbc53f67
Iko <iko@Ikos-MacBook-Air.local>
2025-05-21 23:07:10 +0300

    Updated dotnet version and packages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-
 using F1_Racing_System.Data;
 using Microsoft.EntityFrameworkCore;
 using F1_Racing_System.Mappings;
@@ -6,7 +5,6 @@ using F1_Racing_System.Repositories;
 using System.Text.Json.Serialization;
 using Serilog;
 using Microsoft.AspNetCore.Diagnostics;
-
 
 namespace F1_Racing_System
 {
@@ -26,42 +24,50 @@ namespace F1_Racing_System
             builder.Logging.AddSerilog(logger);
 
             // Add services to the container.
-
             builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddAutoMapper(typeof(AutomapperProfiles));
             builder.Services.AddScoped<ITeamRepository, TeamRepository>();
             builder.Services.AddScoped<IDriverRepository, DriverRepository>();
             builder.Services.AddScoped<IRaceRepository, RaceRepository>();
             builder.Services.AddScoped<IStandingsRepository, StandingsRepository>();
+            
             builder.Services.AddControllers().AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-        // OR to ignore cycles
-        // options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    });
-
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                // OR to ignore cycles
+                // options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
+            // Configure Swagger more explicitly
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            // Make Swagger available regardless of environment
+            // This helps with cross-platform development
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            app.UseMiddleware<Middlewares.ExceptionHandlerMiddleware>();    
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "F1 Racing API V1");
+                // Keep the default route prefix
+                options.RoutePrefix = "swagger";
+            });
+
+            // Add a redirect from root to Swagger UI
+            app.MapGet("/", () => Results.Redirect("/swagger"));
+
+            app.UseMiddleware<Middlewares.ExceptionHandlerMiddleware>();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
+            // Output URLs to console for debugging
+            var urls = app.Urls.ToList();
+            logger.Information("Application starting on URLs: {Urls}", string.Join(", ", urls));
+            
             app.Run();
         }
     }
